b37959644a948f86e74c9515f828ef7836aa7c38
NAIEF-2004 <zeenteeyt@gmail.com>
2025-05-28 19:49:45 +0300

    بعين الله

--- ProjectNews_ASP.NET Core Mvc/Program.cs
+++ ProjectNews_ASP.NET Core Mvc/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProjectNews_ASP.NET_Core_Mvc.Models;
+using Microsoft.AspNetCore.Identity;
+using ProjectNews_ASP.NET_Core_Mvc.Data;
 
 namespace ProjectNews_ASP.NET_Core_Mvc
 {
@@ -9,13 +11,18 @@ namespace ProjectNews_ASP.NET_Core_Mvc
 		{
 
 			var builder = WebApplication.CreateBuilder(args);
+   var connectionString = builder.Configuration.GetConnectionString("ProjectNews_ASPNET_Core_MvcContextConnection") ?? throw new InvalidOperationException("Connection string 'ProjectNews_ASPNET_Core_MvcContextConnection' not found.");
 
 			 
 
 			// Add services to the container.
 
 			builder.Services.AddControllersWithViews();
-			builder.Services.AddDbContext<NewsContext>(options =>
+            
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ProjectNews_ASPNET_Core_MvcContext>();
+
+            
+            builder.Services.AddDbContext<NewsContext>(options =>
 				options.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=ProjectNews_Mvc;Integrated Security=True;"));
 
 			builder.Services.AddControllersWithViews();
